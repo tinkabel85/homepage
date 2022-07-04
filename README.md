3da97feaf83b84f454df90f1836139a253b11182
oksana <opashkovska85@gmail.com>
2022-07-04 11:40:34 +0200

    add README

---
+++ README.md
@@ -0,0 +1,9 @@
+---
+## About the Project Homepage
+
+This is a simple web-application built on pure html/css/javascript. 
+
+Its main idea is to share some information about me, my decision to make a career switch into software development and my learning path towards Front-End Developer career, while practicing my skills in front-end technologies. 
+
+Since I chose web-development, I mostly focused on CSS/HTML, Javascript. ReactJS is also on my radar, however not yet covered on this app. 
+___
--- script.js
+++ script.js
@@ -1,57 +1,56 @@
 const months = [
-    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  const dreamjob = document.querySelector(".dreamjob");
-  const deadline = document.querySelector(".deadline");
-  const items = document.querySelectorAll(".deadline_format h5");
+  "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+]
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const dreamjob = document.querySelector(".dreamjob");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline_format h5");
 
-  const futureDate = new Date(2022, 8, 1, 09, 30);
+const futureDate = new Date(2022, 9, 1, 09, 30);
 
-  const year = futureDate.getFullYear();
-  const hours = futureDate.getHours();
-  const minutes = futureDate.getMinutes();
-  let month = futureDate.getMonth();
-  month = months[month];
-  const date = futureDate.getDate();
-  const weekday = weekdays[futureDate.getDay()];
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
 
-  dreamjob.textContent = `JobOffer by ${weekday}, ${date} ${month} ${year}  ${hours}:${minutes}am`;
+dreamjob.textContent = `JobOffer by ${weekday}, ${date} ${month} ${year}  ${hours}:${minutes}am`;
 
-  /* time in mlsec */
-  const futureTime = futureDate.getTime();
+/* time in mlsec */
+const futureTime = futureDate.getTime();
 
-  function getDifference() {
-    const today = new Date().getTime();
-    const t = futureTime - today;
-    /* ms in one day, hour, min */
-    const oneDay = 24 * 60 * 60 * 1000;
-    const oneHour = 60 * 60 * 1000;
-    const oneMinute = 60 * 1000;
-    let days = Math.floor(t / oneDay);
-    let hours = Math.floor((t % oneDay) / oneHour);
-    let minutes = Math.floor((t % oneHour) / oneMinute);
-    let seconds = Math.floor((t % oneMinute) / 1000);
+function getDifference() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  /* ms in one day, hour, min */
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
 
-    const values = [days, hours, minutes, seconds];
+  const values = [days, hours, minutes, seconds];
 
-    function format(item) {
-      if (item < 10) {
-        return (item = `0${item}`);
-      }
-      return item;
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
     }
-    items.forEach(function(item, index) {
-      item.innerHTML = format(values[index]);
-    });
-    if (t < 0) {
-      clearInterval(countdown);
-      deadline.innerHTML = `<h4 class="
-      expired ">The time is now!</h4>`;
-    }
+    return item;
   }
-  // countdown
-  let countdown = setInterval(getDifference, 1000);
-  getDifference();
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="
+      expired ">The time is now!</h4>`;
+  }
+}
+// countdown
+let countdown = setInterval(getDifference, 1000);
+getDifference();
 
-  
